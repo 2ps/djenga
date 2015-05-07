1ff2c3f29bb454747494f09a9bb42b50806819eb
2ps <preetam.shingavi@ice.com>
2015-05-07 02:17:38 -0700

    Renamed logging directory to `loggers` to avoid naming conflicts.

---
+++ djenga/loggers/__init__.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+from djenga.loggers.util import ColorFormatter
+from djenga.loggers.util import BriefColorFormatter
+from djenga.loggers.util import IgnoreCaching
+from djenga.loggers.util import IgnoreDjangoInternals
+from djenga.loggers.util import IgnorePisaPdf
+from djenga.loggers.util import IgnoreRequests
+from djenga.loggers.util import WorldWritableRotatingFileHandler
