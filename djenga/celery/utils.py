8aa439d518f8683c9a827d442be26ba2458bacbe
pshingavi <p.shingavi@yahoo.com>
2017-06-27 10:51:20 -0500

    [OPS]  Fixed up working of reporting progress
    * my mistake was thinking that the task would be None
      when a task method is called without `delay` or `apply_async`.
    * in fact, the task comes in intact, but there is no associated
      task request id.
    * in these cases, we don't have to update the backend since the
      call did not come through the backend in the first place!

--- djenga/celery/utils.py
+++ djenga/celery/utils.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 import logging
 
 
@@ -9,20 +10,25 @@ logger = logging.getLogger(__name__)
 
 def update_progress(task, progress, *args, **kwargs):
     """
-    Adds progress and additional metadata to a celery task
+    Adds progress and additional metadata to a celery task.  Since
+    celery tasks can also be called as regular ol’ methods, we have
+    to handle cases when task.request.id is None (i.e., task.request.id
+    is None when a task is called as a regular function instead of with
+    `delay` or `apply_async`)
     :param task: celery.Task
     :param progress: format string
     :param args: args for the progress format string
     :param kwargs: additional metadata field/values to set on the task
     """
-    request_id = task.request.id
-    current = getattr(task.request, 'progress', [])
     if args:
         progress %= args
+    request_id = task.request.id
+    current = getattr(task.request, 'progress', [])
     current.append(progress)
-    logger.info(progress)
-    task.backend.mark_as_started(
-        request_id,
-        progress='\n'.join(current),
-        **kwargs)
+    if request_id:
+        task.backend.mark_as_started(
+            request_id,
+            progress='\n'.join(current),
+            **kwargs)
     setattr(task.request, 'progress', current)
+    logger.info(progress)
